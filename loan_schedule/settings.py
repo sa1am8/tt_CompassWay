52a75b794e5d01250e3f6d96f8fa0d0b6c5fa145
sa1am8 <46489947+sa1am8@users.noreply.github.com>
2024-07-09 13:48:24 +0300

    sqlite db used for tests

--- loan_schedule/settings.py
+++ loan_schedule/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
